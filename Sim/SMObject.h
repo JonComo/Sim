fc85a8dba3f8bb4be8df942fa002ccc61496547a
Jon Como <jon@underground.net>
2013-09-01 14:22:51 -0700

    Added object

---
+++ Sim/SMObject.h
@@ -0,0 +1,13 @@
+//
+//  SMObject.h
+//  Sim
+//
+//  Created by Jon Como on 9/1/13.
+//  Copyright (c) 2013 Jon Como. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMObject : NSObject
+
+@end
---
+++ Sim/SMObject.m
@@ -0,0 +1,13 @@
+//
+//  SMObject.m
+//  Sim
+//
+//  Created by Jon Como on 9/1/13.
+//  Copyright (c) 2013 Jon Como. All rights reserved.
+//
+
+#import "SMObject.h"
+
+@implementation SMObject
+
+@end
